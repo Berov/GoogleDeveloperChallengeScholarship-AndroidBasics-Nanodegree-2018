18e0c5f5b3703636d60de316c1235e01616167e6
Berov <nikolai.berov@gmail.com>
2018-02-19 12:10:33 +0200

    Single Screen App

---
+++ FirstProject-SingleScreenApp/README.md
@@ -0,0 +1,30 @@
+# First Project - Single Screen App
+
+PROJECT SPECIFICATION
+
+Your design must include:
+	Business name
+	At least one photo representing the business
+	Two or more other pieces of information, such as:
+		-Contact information for the business (eg phone number, email address, website)
+		-Address of the Business
+		-Description of business
+		-Hours of operation
+
+
+Program must function in Android Studio emulator and on a phone. Upon launching on a phone, the card appears.
+
+
+Layout scales responsively for screen sizes in portrait mode, with no part of any images only partially visible. Student is not responsible for responsive design in landscape mode.
+
+
+Note: Reviewers will test on a Nexus 5X which has a screen size of 5.2 inches (132.08mm). It is advised you test on this size at minimum. If your content does not fit on this size, consider implementing a ScrollView.
+
+
+XML must include a Relative Layout and/or a Linear Layout
+
+
+Must have one or more image views or text views.
+
+
+Elements are specified in dp, text is specified in sp.
--- FirstProject-SingleScreenApp/app/src/main/java/bg/berov/forcepointbar/MainActivity.java
+++ FirstProject-SingleScreenApp/app/src/main/java/bg/berov/forcepointbar/MainActivity.java
@@ -1,13 +1,81 @@
 package bg.berov.forcepointbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView barImage;
+    private CardView barView;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barImage = (ImageView) findViewById(R.id.image_view_picture);
+        barView = (CardView) findViewById(R.id.card_view_bar_view);
+        counter = 1;
+
+        barView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (counter) {
+                    case 1:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp1));
+                        break;
+                    case 2:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp2));
+                        break;
+                    case 3:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp3));
+                        break;
+                    case 4:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp4));
+                        break;
+                    case 5:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp5));
+                        break;
+                    case 6:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp6));
+                        break;
+                    case 7:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp7));
+                        break;
+                    case 8:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp8));
+                        break;
+                    case 9:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp9));
+                        break;
+                    case 10:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp10));
+                        break;
+                    case 11:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp11));
+                        break;
+                    case 12:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp12));
+                        break;
+                    case 13:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp13));
+                        break;
+                    case 14:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp14));
+                        break;
+                    case 15:
+                        barImage.setImageDrawable(getResources().getDrawable(R.drawable.fp15));
+                        counter = 0;
+                        break;
+                }
+                counter++;
+            }
+        });
+
     }
 }
