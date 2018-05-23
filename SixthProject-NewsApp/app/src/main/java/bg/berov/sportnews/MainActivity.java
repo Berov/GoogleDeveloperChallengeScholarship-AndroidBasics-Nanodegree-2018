4828c95082287bf84672d4fb08a253b159033372
Berov <nikolai.berov@gmail.com>
2018-05-23 12:38:57 +0300

    V 1.0 works fine

---
+++ SixthProject-NewsApp/app/src/main/java/bg/berov/sportnews/MainActivity.java
@@ -0,0 +1,111 @@
+package bg.berov.sportnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+//            return PlaceholderFragment.newInstance(position + 1);
+            SectionNewsFragment fragment = new SectionNewsFragment();
+            Bundle bundle = new Bundle();
+            switch (position) {
+                case 0:
+                    bundle.putString(getString(R.string.section__), getString(R.string.sport_1));
+                    break;
+                case 1:
+                    bundle.putString(getString(R.string.section__), getString(R.string.sport_2));
+                    break;
+                case 2:
+                    bundle.putString(getString(R.string.section__), getString(R.string.sport_3));
+                    break;
+                case 3:
+                    bundle.putString(getString(R.string.section__), getString(R.string.sport_4));
+                    break;
+            }
+            bundle.putInt(getString(R.string.section_number_), position);
+            fragment.setArguments(bundle);
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+}
---
+++ SixthProject-NewsApp/app/src/main/java/bg/berov/sportnews/SectionNewsFragment.java
@@ -0,0 +1,162 @@
+package bg.berov.sportnews;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import bg.berov.sportnews.model.NewsItem;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SectionNewsFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<NewsItem>> {
+
+    private TextView state;
+    private ProgressBar progressBar;
+    private ListView listView;
+    String url;
+
+    public SectionNewsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_section_news, container, false);
+
+        Bundle bundle = getArguments();
+        String section = bundle.getString(getString(R.string.section__));
+        url = getString(R.string.first_part_of_the_url) + section + getString(R.string.second_part_of_the_url);
+
+
+        state = (TextView) view.findViewById(R.id.textView_state);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        listView = (ListView) view.findViewById(R.id.listView_news);
+
+
+        ConnectivityManager cm = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        boolean isConnected = networkInfo != null && networkInfo.isConnected();
+
+        if (isConnected) {
+            progressBar.setVisibility(View.VISIBLE);
+            getLoaderManager().initLoader(0, null, this);
+        } else {
+            state.setVisibility(View.VISIBLE);
+            state.setText(R.string.no_connection);
+        }
+
+
+        return view;
+    }
+
+
+    @Override
+    public Loader<List<NewsItem>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(getContext(), url);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<NewsItem>> loader, List<NewsItem> news) {
+
+
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.VISIBLE);
+
+
+        if (news.size() == 0) {
+            state.setText(R.string.no_news);
+            listView.setEmptyView(state);
+        } else {
+            NewsAdapter newsAdapter = new NewsAdapter(getContext(), news);
+            listView.setAdapter(newsAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    NewsItem newsItem = (NewsItem) listView.getItemAtPosition(position);
+                    String url = newsItem.getUrl();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<NewsItem>> loader) {
+        loader.reset();
+    }
+
+
+    //viewHolder for the Atractions adapter
+    private static class ViewHolder {
+        ImageView thumbnail;
+        TextView label, section, author, date;
+    }
+
+    private class NewsAdapter extends ArrayAdapter<NewsItem> {
+        public NewsAdapter(Context context, List<NewsItem> news) {
+            super(context, 0, news);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+
+            // Get the data item for this position
+            NewsItem newsItem = getItem(position);
+
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                // If there's no view to re-use, inflate a brand new view for row
+                viewHolder = new ViewHolder();
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.imageView_thumbnail);
+                viewHolder.label = (TextView) convertView.findViewById(R.id.textView_label);
+                viewHolder.section = (TextView) convertView.findViewById(R.id.textView_section);
+                viewHolder.author = (TextView) convertView.findViewById(R.id.textView_author);
+                viewHolder.date = (TextView) convertView.findViewById(R.id.textView_date);
+
+                // Cache the viewHolder object inside the fresh view
+                convertView.setTag(viewHolder);
+
+            } else {
+                // View is being recycled, retrieve the viewHolder object from tag
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.thumbnail.setImageDrawable(newsItem.getThumbnail());
+            viewHolder.label.setText(newsItem.getLabel());
+            viewHolder.section.setText(getString(R.string.section_)+newsItem.getSection());
+            viewHolder.author.setText(getString(R.string.author_)+newsItem.getAuthor());
+            viewHolder.date.setText(getString(R.string.published_)+newsItem.getDate());
+
+            return convertView;
+        }
+    }
+}
